48cffec8766f8dfc4e69959e04163bb5b370554a
julioluma <elyulius25@gmail.com>
2025-05-20 16:44:36 +0200

    Añadida con bucle de T

--- VoluntarioKawasaki copy.py
+++ VoluntarioKawasaki copy.py
@@ -14,14 +14,16 @@ filename = "estado_inicial.txt" # Nombre del archivo de entrada, debe contener
                                 #   s(2,1), s(2,2), ..., s(2,N)
                                 #   (...)
                                 #   s(N,1), s(N,2), ..., s(N,N)
-T = 2.69  # Temperatura
+                                
+Temperaturas = [1, 1.5, 2, 2.25, 2.5, 2.75, 3] # Lista de temperaturas a simular
 pasos = 10**6  # Número de pasos de Monte Carlo
-Guardar_spines = True  # Guardar el estado de los spines para animar
+Guardar_spines = False  # Guardar el estado de los spines para animar
 pasos_almacenamiento = 100  # Pasos para almacenar el estado de los spines
 pasos_promediar = 100  # Pasos para promediar la energía y la magnetización
 
 # ================================================================================
 # ================================================================================
+
 
 """def inicializar_spines(N, M):
     spines = np.empty((N, N), dtype=np.int32)
@@ -132,7 +134,7 @@ def Kawasaki(spines, beta):
 
     return spines
 
-def simular_ising(filename, T, pasos, pasos_almacenamiento=100, pasos_promediar=1000):
+def simular_ising(filename, T, pasos, pasos_almacenamiento=100, pasos_promediar=100):
     """Simula el modelo de Ising."""
     if not os.path.exists(filename):
         raise FileNotFoundError(f"El archivo {filename} no existe.")
@@ -164,31 +166,28 @@ def simular_ising(filename, T, pasos, pasos_almacenamiento=100, pasos_promediar=
 
 
             spines = Kawasaki(spines, beta)
-            energia = energia_total(spines)
-            magnetizacion1, magnetizacion2 = magnetizacion_promedio(spines, M)
-            energias.append(energia)
-            magnetizaciones1.append(magnetizacion1)
-            magnetizaciones2.append(magnetizacion2)
+            
+            if paso % pasos_promediar == 0:
+                energia = energia_total(spines)
+                magnetizacion1, magnetizacion2 = magnetizacion_promedio(spines, M)
+                energias.append(energia)
+                magnetizaciones1.append(magnetizacion1)
+                magnetizaciones2.append(magnetizacion2)
 
             if Guardar_spines:
                 if paso % pasos_almacenamiento == 0:
                     # Guardar el estado de los spines en cada x pasos
                     guardar_spines_txt(spines, paso)
 
-            if paso % pasos_promediar == 0:
-                M1.append(np.mean(magnetizaciones1))
-                M2.append(np.mean(magnetizaciones2))
-                Sus.append(np.var(magnetizaciones1)/(N1*N*T))
-                magnetizaciones1 = []
-                magnetizaciones2 = []
-                E.append(np.mean(energias)/N**2)
-                Cv.append(np.var(energias)/N**2*T**2)
-                energias = []
+        M1 = np.mean(magnetizaciones1)
+        M2 = np.mean(magnetizaciones2)
+        Sus = np.var(magnetizaciones1)/(N1*N*T)
+        E = np.mean(energias)/N**2
+        Cv = np.var(energias)/N**2*T**2
 
 
     return N, M, spines, E, M1, M2, Cv, Sus
 
-inicio = time.time()
 
 
 @nb.njit
@@ -210,68 +209,61 @@ def dist_densidad(spines):
         dist[i] = dist[i] / N
     return dist, densidad
 # ================================================================================
+inicio = time.time()
+EVec = []
+M1Vec = []
+M2Vec = []
+CvVec = []
+SusVec = []
 
 
-N, M, spines_final, E, M1, M2, Cv, Sus = simular_ising(filename, T, pasos, pasos_almacenamiento, pasos_promediar)
-distribucion_densidad, densidad = dist_densidad(spines_final)
-Nvector = np.arange(0, N)
+for T in Temperaturas:
+    # Ejecutar la simulación
+    print(f"Simulando para T={T}...")
+    
+
+    N, M, spines_final, E, M1, M2, Cv, Sus = simular_ising(filename, T, pasos, pasos_almacenamiento, pasos_promediar)
+    distribucion_densidad, densidad = dist_densidad(spines_final)
+    Nvector = np.arange(0, N)
+
+    EVec.append(E)
+    M1Vec.append(M1)
+    M2Vec.append(M2)
+    CvVec.append(Cv)
+    SusVec.append(Sus)
+
 
 fin = time.time()
-
 # Mostrar el tiempo de ejecución
 print(f"Tiempo de ejecución: {fin - inicio:.2f} s")
-print(f"Susceptibilidad magnética: {Sus[-1]}")
-print(f"Calor específico: {Cv[-1]}")
-
 # Graficar resultados
 ax = plt.figure(figsize=(12, 5))
 ax.suptitle(f"N={N}, T={T}, pMc={pasos}, M={M}\n Tiempo de ejecución: {fin - inicio:.2f} s")
-
-
-plt.subplot(2, 3, 1)
-plt.plot(M1, label="Magnetización promedio superior")
-plt.plot(M2, label="Magnetización promedio inferior")
-plt.xlabel("Paso")
+plt.subplot(2, 2, 1)
+plt.plot(Temperaturas, M1Vec, label="Magnetización promedio superior")
+plt.plot(Temperaturas, M2Vec, label="Magnetización promedio inferior")
+plt.xlabel("Temperatura")
 plt.ylabel("Magnetización")
 plt.legend()
-
-
-plt.subplot(2, 3, 2)
-plt.plot(E, label="Energía promedio de una partícula")
-plt.xlabel("Paso")
+plt.subplot(2, 2, 2)
+plt.plot(Temperaturas, EVec, label="Energía promedio de una partícula")
+plt.xlabel("Temperatura")
 plt.ylabel(r"$\varepsilon$")
 plt.legend()
-
-
-plt.subplot(2, 3, 4)
-plt.plot(range(100,pasos//pasos_promediar), Sus[100:], label="Susceptibilidad magnética")
-plt.xlabel("Paso")
+plt.subplot(2, 2, 3)
+plt.plot(Temperaturas, SusVec, label="Susceptibilidad magnética")
+plt.xlabel("Temperatura")
 plt.ylabel(r"$\chi_N$")
 plt.legend()
-
-
-plt.subplot(2, 3, 5)
-plt.plot(range(100,pasos//pasos_promediar), Cv[100:], label="Calor específico")
-plt.xlabel("Paso")
+plt.subplot(2, 2, 4)
+plt.plot(Temperaturas, CvVec, label="Calor específico")
+plt.xlabel("Temperatura")
 plt.ylabel(r"$c_v$")
 plt.legend()
-
-plt.subplot(2, 3, 3)
-plt.bar(Nvector, distribucion_densidad, label="Distribución de densidades")
-plt.xlabel("Número de spines hacia arriba por columna")
-plt.ylabel("Densidad")
-plt.legend()
-
-plt.subplot(2, 3, 6)
-plt.imshow(spines_final, cmap="coolwarm", interpolation="nearest")
-plt.title(f"Configuración final del spines para T={T}")
-plt.axis("off")
-plt.colorbar(label="Spin")
-
 # Crear la carpeta "Figuras" si no existe
 if not os.path.exists("Figuras"):
     os.makedirs("Figuras")
 # Guardar el gráfico en la carpeta "Figuras"
 plt.tight_layout()
-plt.savefig(f"Figuras/N={N}, T={T}, pMc={pasos}, M={M}.png", dpi=300, bbox_inches="tight")
+plt.savefig(f"Figuras/N={N}_pMc={pasos}_M={M}.png", dpi=300, bbox_inches="tight")
 plt.show()
